ce28bed587134d5f83d96f29ba041bca9c84f710
Denis <denis.inghelbrecht@outlook.be>
2020-10-26 20:34:06 +0100

    create header component and create index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,17 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "./header/Header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const {
-    data: {
-      site: {
-        siteMetadata: { title },
-      },
-    },
-  } = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -35,8 +29,8 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: "auto",
+          padding: 0,
         }}
       >
         <main>{children}</main>
